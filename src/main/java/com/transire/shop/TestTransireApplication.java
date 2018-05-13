8252d98be4136fe6b8c3e282af331251c64effad
Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2018-05-13 10:21:56 -0400

    Start project's Sprint Boot

---
+++ src/main/java/com/transire/shop/TestTransireApplication.java
@@ -0,0 +1,12 @@
+package com.transire.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTransireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTransireApplication.class, args);
+	}
+}
