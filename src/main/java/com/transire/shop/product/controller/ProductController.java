76a1b4d4849fc5f24c329252a075bfdc34f86960
Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2018-05-13 22:44:16 -0400

    Enable CORS domain

--- src/main/java/com/transire/shop/product/controller/ProductController.java
+++ src/main/java/com/transire/shop/product/controller/ProductController.java
@@ -11,6 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class ProductController {
 
